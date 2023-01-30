a509a46ec192182cfc6c266b6a26bdadb3546d7c
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-01-30 22:56:23 +0100

    Create App.jsx

---
+++ src/components/App/App.jsx
@@ -0,0 +1,31 @@
+import { lazy, Suspense } from 'react';
+import { Route, Routes, NavLink, Navigate } from 'react-router-dom';
+//import s from './App.module.css';
+import Navigation from '../Navigation/Navigation';
+
+const HomePage = lazy(() => import('../../pages/HomePage/HomePage'));
+const MoviesPage = lazy(() => import('../../pages/MoviesPage/MoviesPage'));
+const MovieDetailsPage = lazy(() =>
+  import('../../pages/MovieDetailsPage/MovieDetailsPage')
+);
+const Cast = lazy(() => import('../Cast/Cast'));
+const Reviews = lazy(() => import('../Reviews/Reviews'));
+
+export const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <Suspense fallback="loading">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="movies" element={<MoviesPage />} />
+          <Route path="movies/:movieId" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
+};
