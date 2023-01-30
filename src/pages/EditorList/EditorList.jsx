2516378abe033a56651ad57fc2846cee89f7fc8f
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-01-30 22:56:43 +0100

    Create EditorList.jsx

---
+++ src/pages/EditorList/EditorList.jsx
@@ -0,0 +1,31 @@
+import { Link, useLocation } from 'react-router-dom';
+import s from './EditorList.module.css'
+import PropTypes from 'prop-types';
+
+const EditorList = ({ films }) => {
+  const location = useLocation();
+
+  return (
+    <ul className={s.moviesList}>
+      {films &&
+        films.map(film => (
+          <li className={s.moviesList__item} key={film.id}>
+            <Link to={`/movies/${film.id}`} state={{ from: location }}>
+              {film.title}
+            </Link>
+          </li>
+        ))}
+    </ul>
+  );
+};
+
+EditorList.propTypes = {
+  films: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ),
+};
+
+export default EditorList;
